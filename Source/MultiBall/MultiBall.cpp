00f77b2bb0ff58141f1bcaaa305e453acfc9587c
qirui <hlfqirui@gmail.com>
2026-03-13 19:52:37 -0400

    Initial commit

---
+++ Source/MultiBall/MultiBall.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MultiBall.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MultiBall, "MultiBall" );
